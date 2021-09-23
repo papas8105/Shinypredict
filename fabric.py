2324bdeff66c8d11aa0ff48e8620e41d99ced9cf
Pablo Leon <76554081+Pablo-source@users.noreply.github.com>
2021-09-23 08:14:59 +0100

    Create fabric.py
    Fabric script to run simultaneously commands on Raspberry pi cluster

---
+++ fabric.py
@@ -0,0 +1,29 @@
+
+# Fabric library to send commands to Raspberry pi cluster
+# 1. First wrap up all all Raspberry pi cluster's IP using env.host()
+# 2. Then send any commands to the cluster using fab cmd "command"
+
+
+
+from fabric.api import *
+    env.host = [
+
+        'pi@192.168.1.64',
+        'pi@192.168.1.64',
+        'pi@192.168.1.64',
+        'pi@192.168.1.64',
+    ]
+    env.password='raspberry'
+
+
+    #dasds
+    ##dasd
+    ## fab cmd:"echo pi:pai1978|chpasswd"
+    ## echo pi:t0p3cr3t|chpasswd
+    ## fab cmd "apt update"
+    ## fab cmd "apt upgrade"
+    ## fab cmd "apt dist-upgrade-y"
+    ## fab cmd "raspi-config--expand--rootfs"
+    ## fab cmd "reboot now"
+
+    
